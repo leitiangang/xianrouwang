92228d6f2a8c8e7375e44307e8f09b43deeff805
leitiangang <leigang552078114@qq.com>
2016-10-20 10:11:48 +0800

    4444

--- src/main/java/xianrouwang/A.java
+++ src/main/java/xianrouwang/A.java
@@ -3,5 +3,7 @@ package xianrouwang;
 public class A {
 public static void main(String[] args) {
 	System.out.println("");	System.out.println("aa");
-	System.out.println("dd");}
+	System.out.println("dd");
+	Integer b=1;
+	}
 }
